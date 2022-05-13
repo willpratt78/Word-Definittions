74f3bf8fa0f38f791e942a5ee38c634a972095ba
William Pratt <willpratt78@gmail.com>
2022-05-13 14:02:28 -0700

    add tests and code to definition"

--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -5,11 +5,77 @@ require 'pry'
 
 describe '#Definition' do
 
-  describe('#save') do 
-    it('saves a definition') do
-      definition = Definition.new("a defitnion", @word.id, nil)
+  before(:each) do
+    Word.clear()
+    Definition.clear()
+    @word = Word.new("Giant Steps", nil)
+    @word.save()
+  end
+
+  describe('#==') do
+    it("is the same definition if it has the same attributes as another defintion") do
+      definition = Definition.new("Naima", @word.id, nil)
+      definition2 = Definition.new("Naima", @word.id, nil)
+      expect(definition).to(eq(definition2))
+    end
+  end
+
+  describe('.all') do
+    it("returns a list of all songs") do
+      definition = Definition.new("Giant Steps", @word.id, nil)
       definition.save()
-      expect(Defintion.all).to(eq([definition]))
+      definition2 = Definition.new("Naima", @word.id, nil)
+      definition2.save()
+      expect(Definition.all).to(eq([definition, definition2]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all definitions") do
+      definition = Definition.new("Giant Steps", @word.id, nil)
+      definition.save()
+      definition2 = Defintion.new("Naima", @word.id, nil)
+      definition2.save()
+      Definition.clear()
+      expect(Definition.all).to(eq([]))
+    end
+  end
+
+  describe('#save') do
+    it("saves a definition") do
+      definition = Definition.new("Naima", @word.id, nil)
+      definition.save()
+      expect(Definition.all).to(eq([definition]))
+    end
+  end
+
+  describe('.find') do
+    it("finds a defintition by id") do
+      definition = Definition.new("Giant Steps", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("Naima", @word.id, nil)
+      definition2.save()
+      expect(Definition.find(definition.id)).to(eq(definition))
+    end
+  end
+
+  describe('#update') do
+    it("updates an definition by id") do
+      definition = Definition.new("Naima", @word.id, nil)
+      definition.save()
+      definition.update("Mr. P.C.", @word.id)
+      expect(definition.name).to(eq("Mr. P.C."))
+    end
+  end
+
+  describe('#delete') do
+    it("deletes an definition by id") do
+      definition = Definition.new("Giant Steps", @word.id, nil)
+      definition.save()
+      definition2 = Definition.new("Naima", @word.id, nil)
+      definition2.save()
+      definition.delete()
+      expect(Definition.all).to(eq([definition2]))
     end
   end
 end
